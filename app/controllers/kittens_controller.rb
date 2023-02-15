c2d4a2b8cc32f6529f2e9e542f8cb9f45b11ef81
Cathy Rolfs <crolfs47@gmail.com>
2023-02-15 10:23:21 -0600

    Add index action and view to list all kittens

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,23 @@
 class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
